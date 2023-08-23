04019dd85f28c71aa67bba2567cd0b67d9dce287
thanh.tieulythanh@gmail.com <thanh.tieulythanh@gmail.com>
2023-08-23 11:21:33 +0700

    up
    Co-Authored-By: duyenkoz <140132101+duyenkoz@users.noreply.github.com>

---
+++ js/value.js
@@ -0,0 +1,55 @@
+let sections = document.querySelectorAll('section');
+window.addEventListener('scroll', () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add('show-animate');
+    } else {
+      sec.classList.remove('show-animate');
+    }
+  });
+});
+
+window.onload = function() {
+    const accordionItems = document.querySelectorAll('.value_accordion-item')
+
+    accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value_accordion-header')
+    accordionHeader.addEventListener('click', () =>{ const openItem = document.querySelector('.accordion-open')
+    toggleItem(item)
+    if (openItem && openItem!== item) { 
+        toggleItem(openItem)
+    }
+
+    })
+
+})
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value_accordion-content')
+
+    if(item.classList.contains('accordion-open')){ 
+        accordionContent.removeAttribute('style') 
+        item.classList.remove('accordion-open')
+
+}else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px' 
+    item.classList.add('accordion-open')
+}
+}
+
+
+
+let t = document.getElementById("myMenuFunction")
+    t.onclick = function () {
+    var i = document.getElementById("navMenu");
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+}
+
